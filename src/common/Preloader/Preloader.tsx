47ca900a7c644a2a01b2b915f38cfc5e03fa3ab8
eliifgokdemiir <gokdemirelif12@gmail.com>
2025-01-29 10:21:15 +0300

    laoding logo changed

--- src/common/Preloader/Preloader.tsx
+++ src/common/Preloader/Preloader.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import logoText from "../../../public/assets/img/logo/logo-text.png";
+import logoText from "../../../public/assets/img/logo/mazali-seffaf.png";
 import preloaderImg from "../../../public/assets/img/logo/preloader.svg";
 
 const Preloader = () => {
@@ -11,8 +11,25 @@ const Preloader = () => {
             <div id="loading-center">
                 <div id="loading-center-absolute">
                     <div className="loading-icon text-center d-flex flex-column align-items-center justify-content-center">
-                    <Image src={logoText} alt="logo"/>
-                    <Image className="loading-logo" src={preloaderImg} alt="loading-logo"/>
+                        <Image 
+                            src={logoText} 
+                            alt="logo"
+                            style={{
+                                width: '300px',
+                                height: '250px',
+                                marginBottom: '0'
+                            }}
+                        />
+                        <Image 
+                            className="loading-logo" 
+                            src={preloaderImg} 
+                            alt="loading-logo"
+                            style={{
+                                width: '80px',
+                                height: 'auto',
+                                marginTop: '0'
+                            }}
+                        />
                     </div>
                 </div>
             </div>
