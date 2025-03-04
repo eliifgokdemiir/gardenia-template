ec983224f76f5f119da5c4ee64e25ee87cc2c271
eliifgokdemiir <gokdemirelif12@gmail.com>
2025-03-04 15:30:59 +0300

    minor changes

--- src/data/pastane-products-data.ts
+++ src/data/pastane-products-data.ts
@@ -17,78 +17,107 @@ export const pastryProducts: PastaneProduct[] = [
     {
         id: 2,
         title: "ARA SICAKLAR",
-        img: "/assets/img/product/sufle.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Günün her saati keyifle tüketebileceğiniz lezzetli ara sıcaklarımız!",
         price: ""
     },
     {
         id: 3,
         title: "ANA YEMEKLER",
-        img: "/assets/img/product/pasta.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Özenle hazırlanan ana yemek seçeneklerimiz ile doyurucu bir öğün deneyimi yaşayın. Özel soslarla hazırlanan et ve tavuk yemekleri, mevsim sebzeleriyle sunulan tarifler.",
         price: ""
     },
     {
         id: 4,
         title: "FAST FOOD",
-        img: "/assets/img/product/pasta1.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Hızlı ama lezzetli seçenekler!",
         price: ""
     },
     {
         id: 5,
         title: "DÜNYA MUTFAĞI LEZZETLERİ",
-        img: "/assets/img/product/pasta2.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "İtalyan, Fransız, Uzak Doğu ve Akdeniz mutfaklarından özenle seçilmiş global lezzetler. Farklı kültürlerin eşsiz tatlarını keşfedin.",
         price: ""
     },
     {
         id: 6,
         title: "TATLILAR",
-        img: "/assets/img/product/pasta2.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Tatlı tutkunları için özel reçetelerle hazırlanan enfes lezzetler: Sütlü tatlılar, çikolatalı tatlılar, geleneksel Türk tatlıları ve dünya tatlıları...",
         price: ""
     },
     {
         id: 7,
         title: "YAŞ PASTALAR",
-        img: "/assets/img/product/pasta2.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Özel günlerinizi taçlandıracak, usta pastacılarımızın elinden çıkan nefis yaş pastalar: Meyveli, çikolatalı, frambuazlı ve özel tasarım pastalar.",
         price: ""
     },
     {
         id: 8,
         title: "UNLU MAMÜLLER",
-        img: "/assets/img/product/pasta2.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Günlük taze pişirilen ekmekler, poğaçalar, açmalar, börekler ve çörekler. Geleneksel tarifler ve modern dokunuşlarla hazırlanan unlu mamüllerimiz.",
         price: ""
     },
     {
         id: 9,
         title: "EKŞİ MAYA ÜSTÜ LEZZETLER",
-        img: "/assets/img/product/pasta2.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Ekşi maya hamuruyla hazırlanan özel lezzetler: Pizzalar, ekmekler, focaccia ve daha fazlası. Doğal fermentasyon ile hazırlanan sağlıklı ve lezzetli seçenekler.",
         price: ""
     },
     {
         id: 10,
         title: "KAHVE ÇEŞİTLERİ",
-        img: "/assets/img/product/pasta2.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Dünyanın dört bir yanından özenle seçilmiş kahve çekirdekleri ile hazırlanan espresso bazlı içecekler, filtre kahveler, soğuk demleme kahveler ve özel karışımlar.",
         price: ""
     },
     {
         id: 11,
         title: "PAKET ÜRÜNLER",
-        img: "/assets/img/product/pasta2.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Evinizde de Mazali lezzetlerini yaşamanız için özel olarak hazırlanan paket ürünlerimiz: Kurabiyeler, kekler, çikolatalar, reçeller ve özel karışımlar.",
         price: ""
     },
     {
         id: 12,
         title: "ATIŞTIRMALIKLAR",
-        img: "/assets/img/product/pasta2.jpg",
+        img: "/assets/img/product/meyvelipasta.jpg",
         desc: "Kahvenizin yanında veya gün içi atıştırmalık olarak tercih edebileceğiniz lezzetler: Kurabiyeler, brownie'ler, muffin'ler, kruvasan ve daha fazlası...",
         price: ""
-    }
+    },
+    {
+        id: 13,
+        title: "KURABİYELER",
+        img: "/assets/img/product/meyvelipasta.jpg",
+        desc: "Günün her saati keyifle tüketebileceğiniz lezzetli kurabiyelerimiz!",
+        price: ""
+    },
+    {
+        id: 14,
+        title: "LİMONATALAR",
+        img: "/assets/img/product/meyvelipasta.jpg",
+        desc: "Taze sıkılmış limon suyu ile hazırlanan limonatalar, günün her saati keyifle tüketebileceğiniz lezzetli limonatalarımız!",
+        price: ""
+    },
+    {
+        id: 15,
+        title: "SOĞUK İÇECEKLER",
+        img: "/assets/img/product/meyvelipasta.jpg",
+        desc: "Günün her saati keyifle tüketebileceğiniz lezzetli soğuk içeceklerimiz!",
+        price: ""
+    },
+    {
+        id: 16,
+        title: "SICAK İÇECEKLER",
+        img: "/assets/img/product/meyvelipasta.jpg",
+        desc: "Günün her saati keyifle tüketebileceğiniz lezzetli sıcak içeceklerimiz!",
+        price: ""
+    },
+ 
 ]; 
